0eabe6e9aa3141b5ced61d4974ca24aaa7b44268
Anton Pomogaev <antonpomogaev@gmail.com>
2022-05-21 22:32:56 +0300

    feat: add solution for 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const { readdir, mkdir, copyFile, rm } = require('fs/promises');
+const path = require('path');
 
+async function copyFolder(
+  pathToSrcFolder,
+  pathToDestFolder,
+  options = { recursive: false, destFolderCleared: false }
+) {
+  try {
+    if (!options.destFolderCleared) {
+      await rm(pathToDestFolder, { recursive: true, force: true });
+    }
+
+    await mkdir(pathToDestFolder);
+
+    const dirEntries = await readdir(pathToSrcFolder, {
+      withFileTypes: true,
+    });
+
+    for (const dirent of dirEntries) {
+      if (dirent.isFile()) {
+        const pathToSrcFile = path.join(pathToSrcFolder, dirent.name);
+        const pathToDestFile = path.join(pathToDestFolder, dirent.name);
+
+        copyFile(pathToSrcFile, pathToDestFile);
+      }
+
+      if (dirent.isDirectory() && options.recursive) {
+        copyFolder(
+          path.join(pathToSrcFolder, dirent.name),
+          path.join(pathToDestFolder, dirent.name),
+          { recursive: true, destFolderCleared: true }
+        );
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+copyFolder(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'), {
+  recursive: true,
+});
