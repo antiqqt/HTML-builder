517ff74c63761905fd127b370189bf7834fe3593
Anton Pomogaev <antonpomogaev@gmail.com>
2022-05-22 22:10:57 +0300

    feat: add solution for 5th task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const { createWriteStream, createReadStream } = require('fs');
+const { readdir } = require('fs/promises');
+const path = require('path');
+
+(async () => {
+  try {
+    const bundleWriteStream = createWriteStream(
+      path.join(__dirname, 'project-dist', 'bundle.css')
+    );
+
+    const stylesDirEntries = await readdir(path.join(__dirname, 'styles'), {
+      withFileTypes: true,
+    });
+
+    for (const dirent of stylesDirEntries) {
+      if (!dirent.isFile() || !dirent.name.endsWith('.css')) continue;
+
+      const styleReadStream = createReadStream(
+        path.join(__dirname, 'styles', dirent.name)
+      );
+
+      styleReadStream.pipe(bundleWriteStream);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+})();
